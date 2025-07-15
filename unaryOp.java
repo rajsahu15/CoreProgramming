e01ac37b6bf11bc26bc267e4b535ee5681116a97
rajsahu15 <rajsahuwork@mail.com>
2025-07-15 21:13:07 +0530

    UnaryOperator

---
+++ unaryOp.java
@@ -0,0 +1,9 @@
+public class unaryOp {
+    public static void main(String[] args) {
+        int a=10;
+        a=a++;
+        System.out.println(a);
+        int b=a++;
+        System.out.println(b);
+    }
+}
